1b8fef075e362071f003c8494ac7949180bce1cc
Ashraf <ashrafmalik91496@gmail.com>
2026-06-03 15:32:12 +0530

    Delete start.tsx

--- start.tsx
+++
@@ -1,6 +0,0 @@
-import { StartClient, createBrowserHistory } from "@tanstack/react-start";
-import { createRouter } from "./router";
-
-const router = createRouter({ history: createBrowserHistory() });
-
-StartClient({ router });
